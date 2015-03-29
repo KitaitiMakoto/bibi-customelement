6933ceb2e2f67b70eb2285a555c46f21bb3c8e32
KITAITI Makoto <KitaitiMakoto@gmail.com>
2015-03-29 19:46:53 +0900

    [WIP]

--- src/e-book.js
+++ src/e-book.js
@@ -1,20 +1,119 @@
-(function() {
-    // Creates an object based in the HTML Element prototype
-    var element = Object.create(HTMLElement.prototype);
+(global) => {
+    "use strict";
 
-    // Fires when an instance of the element is created
-    element.createdCallback = function() {};
+    var templateContent = document.currentScript.ownerDocument.getElementsByTagName("template")[0].content;
+    var uri = document.createElement("link");
 
-    // Fires when an instance was inserted into the document
-    element.attachedCallback = function() {};
+    function defineValueAttributes(proto, attrs) {
+        attrs.forEach(attrName => {
+            Object.defineProperty(proto, attrName, {
+                enumerable: true,
+                get: function() {
+                    return this.getAttribute(attrName);
+                },
+                set: function(value) {
+                    this.setAttribute(attrName, value);
+                }
+            })
+        });
+    };
 
-    // Fires when an instance was removed from the document
-    element.detachedCallback = function() {};
+    function definePredicateAttributes(proto, attrs) {
+        attrs.forEach(attrName => {
+            Object.defineProperty(proto, attrName, {
+                enumerable: true,
+                get: function() {
+                    return this.hasAttribute(attrName);
+                },
+                set: function(value) {
+                    if (value) {
+                        this.setAttribute(attrName, "");
+                    } else {
+                        this.removeAttribute(attrName);
+                    }
+                }
+            });
+        });
+    };
 
-    // Fires when an attribute was added, removed, or updated
-    element.attributeChangedCallback = function(attr, oldVal, newVal) {};
+    function loadEbook(element, src) {
+        console.warn(`${element.tagName.toLowerCase()}'s src attributes's format will be changed in the future`);
+        if (! src) {
+            return;
+        }
+        var root = element.shadowRoot;
+        var holder = root.querySelector(".holder")
+        var frame = root.querySelector("iframe");
+        var frameSrc = src;
+        var switchFullscreen = root.querySelector(".switch-fullscreen");
+        var switchNewwindow = root.querySelector(".switch-newwindow");
+        var poster = element.poster;
+        if (element.autostart) {
+            frameSrc += (/#/.test(frameSrc) ? "," : "#") + "pipi(autostart:true)";
+        } else if(poster) {
+            uri.href = poster;
+            if (poster !== uri.href) {
+                element.poster = uri.href;
+            }
+            frameSrc += (/#/.test(frameSrc) ? "," : "#") + "pipi(poster:" + encodeURIComponent(poster).replace("(", "_BibiKakkoOpen_").replace(")", "_BibiKakkoClose_") + ")";
+        }
+        frame.addEventListener("load", function(event) {
+            element.dispatchEvent(new event.constructor(event.type, event));
+            var iframe = event.target;
+            var waiting = false;
+            var observer = new MutationObserver(mutations => {
+                mutations.forEach(mutation => {
+                    if (mutation.attributeName !== "class") {
+                        return;
+                    }
+                    if (mutation.target.classList.contains("wait-please") && (! waiting)) {
+                        waiting = true;
+                    }
+                    if (! mutation.target.classList.contains("wait-please") && waiting) {
+                        waiting = undefined;
+                        ["canplay", "canplaythrough", "play"].forEach(eventName => {
+                            element.dispatchEvent(new CustomEvent(eventName));
+                        });
+                        observer.disconnect();
+                    }
+                });
+            });
+            observer.observe(iframe.contentWindow.document.getElementsByTagName("html")[0], {attributes: true});
+        });
+        frame.src = frameSrc;
+        switchNewwindow.href = src;
+    }
 
-    document.registerElement('e-book', {
-        prototype: element
-    });
-}());
+    class HTMLEBookElement extends HTMLElement {
+        set target(value) {
+            console.warn("TODO: change target window");
+        }
+
+        createdCallback() {
+            var root = this.createShadowRoot();
+            root.appendChild(document.importNode(templateContent, true));
+            if (this.src) {
+                loadEbook(this, this.src);
+            }
+
+        }
+
+        attributeChangedCallback(name, oldValue, newValue, namespace) {
+            if ((name === "src") && (oldValue !== newValue)) {
+                loadEbook(this, newValue);
+            }
+        }
+    }
+    for (let prop of ["requestFullscreen", "msRequestFullscreen", "mozRequestFullscreen", "webkitRequestFullscreen"]) {
+        var requestFullscreen = HTMLEBookElement.prototype[prop];
+        if (requestFullscreen) {
+            HTMLEBookElement.prototype.requestFullscreen = requestFullscreen;
+            break;
+        }
+    };
+
+    defineValueAttributes(HTMLEBookElement.prototype, ["src", "poster", "type"]);
+    definePredicateAttributes(HTMLEBookElement.prototype, ["autostart"]);
+
+    global.HTMLEBookElement = document.registerElement("e-book", HTMLEBookElement);
+}(self);
